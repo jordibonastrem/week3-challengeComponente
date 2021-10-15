766890db764f03538d176e3f7591c0161b71fa12
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-10-15 09:51:00 +0200

    initial files

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    requireConfigFile: false,
+  },
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+    "lines-between-class-members": "off",
+    "import/extensions": "off",
+  },
+  parser: "@babel/eslint-parser",
+};
